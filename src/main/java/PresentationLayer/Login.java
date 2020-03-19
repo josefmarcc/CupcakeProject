52d10dee35cea237c1f59756df1dec4c808c75d9
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2020-03-19 13:00:06 +0100

    Opdateret login

--- src/main/java/PresentationLayer/Login.java
+++ src/main/java/PresentationLayer/Login.java
@@ -24,7 +24,12 @@ public class Login extends Command {
 
         session.setAttribute("email", email);  // ellers skal man skrive  user.email på jsp siderne og det er sgu lidt mærkeligt at man har adgang til private felter. Men måske er det meget fedt , jeg ved det ikke
         session.setAttribute("credit", user.showBalance());
-        return "../index";
+
+        switch (email){
+            case "admin": return "admin";
+            default: return "../index";
+        }
+
     }
 
 }
