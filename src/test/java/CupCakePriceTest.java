7cd740a192cc863a8cea376c1100b0718b4e9ebd
Frederik Dahl <fdah@itu.dk>
2020-03-24 10:21:15 +0100

    Test af cupcake price

--- src/test/java/CupCakePriceTest.java
+++ src/test/java/CupCakePriceTest.java
@@ -1,14 +1,14 @@
-package Util;
-
 import FunctionLayer.Bottom;
 import FunctionLayer.Cupcake;
 import FunctionLayer.Topping;
-
+import Util.CupcakePrice;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CupCakePriceTest {
 
-class CupcakePriceTest {
     CupcakePrice cp = new CupcakePrice();
     Cupcake cupcake1;
     Cupcake cupcake2;
@@ -16,13 +16,12 @@ class CupcakePriceTest {
     Cupcake cupcake4;
     ArrayList<Cupcake> cupcakeList = new ArrayList<>();
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
-
-        Topping top1 = new Topping("Chokolade",5);
-        Topping top2 = new Topping("Banan",7);
-        Topping top3 = new Topping("Jordbær",6);
-        Topping top4 = new Topping("Hindbær",6);
+        Topping top1 = new Topping("Chokolade", 5);
+        Topping top2 = new Topping("Banan", 7);
+        Topping top3 = new Topping("Jordbær", 6);
+        Topping top4 = new Topping("Hindbær", 6);
         Bottom bot1 = new Bottom("Chokolade", 5);
         Bottom bot2 = new Bottom("Banan", 5);
         Bottom bot3 = new Bottom("Jordbær", 5);
@@ -37,14 +36,15 @@ class CupcakePriceTest {
         cupcakeList.add(cupcake4);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void calculateCupcakePriceTest() {
         double result = cp.calculateCupcakePrice(cupcake1, 4);
         double expected = 40;
         assertEquals(result, expected);
-
     }
-    @org.junit.jupiter.api.Test
+
+
+    @Test
     void calculateBasketPrice() {
         ArrayList<Integer> qtylist = new ArrayList<>();
         qtylist.add(2);
@@ -58,3 +58,4 @@ class CupcakePriceTest {
         assertEquals(expected, result);
     }
 }
+
