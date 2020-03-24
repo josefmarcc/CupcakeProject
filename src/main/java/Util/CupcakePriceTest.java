73172c95e57584ca9a4b1ef3a061903b3b0975be
Frederik Dahl <fdah@itu.dk>
2020-03-24 09:38:04 +0100

    test

---
+++ src/main/java/Util/CupcakePriceTest.java
@@ -0,0 +1,60 @@
+package Util;
+
+import FunctionLayer.Bottom;
+import FunctionLayer.Cupcake;
+import FunctionLayer.Topping;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CupcakePriceTest {
+    CupcakePrice cp = new CupcakePrice();
+    Cupcake cupcake1;
+    Cupcake cupcake2;
+    Cupcake cupcake3;
+    Cupcake cupcake4;
+    ArrayList<Cupcake> cupcakeList = new ArrayList<>();
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+        Topping top1 = new Topping("Chokolade",5);
+        Topping top2 = new Topping("Banan",7);
+        Topping top3 = new Topping("Jordbær",6);
+        Topping top4 = new Topping("Hindbær",6);
+        Bottom bot1 = new Bottom("Chokolade", 5);
+        Bottom bot2 = new Bottom("Banan", 5);
+        Bottom bot3 = new Bottom("Jordbær", 5);
+        Bottom bot4 = new Bottom("Hindbær", 5);
+        cupcake1 = new Cupcake(top1, bot1);
+        cupcake2 = new Cupcake(top2, bot2);
+        cupcake3 = new Cupcake(top3, bot3);
+        cupcake4 = new Cupcake(top4, bot4);
+        cupcakeList.add(cupcake1);
+        cupcakeList.add(cupcake2);
+        cupcakeList.add(cupcake3);
+        cupcakeList.add(cupcake4);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateCupcakePriceTest() {
+        double result = cp.calculateCupcakePrice(cupcake1, 4);
+        double expected = 40;
+        assertEquals(result, expected);
+
+    }
+    @org.junit.jupiter.api.Test
+    void calculateBasketPrice() {
+        ArrayList<Integer> qtylist = new ArrayList<>();
+        qtylist.add(2);
+        qtylist.add(1);
+        qtylist.add(2);
+        qtylist.add(3);
+
+        double expected = 87;
+        double result = cp.calculateBasketPrice(cupcakeList, qtylist);
+
+        assertEquals(expected, result);
+    }
+}
