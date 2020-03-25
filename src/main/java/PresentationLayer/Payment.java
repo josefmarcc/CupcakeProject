81cde028d4fa9144e8f42d0066e53b087aab5821
Frederik Dahl <fdah@itu.dk>
2020-03-25 09:48:42 +0100

    Quick inputvalidation ved payment

--- src/main/java/PresentationLayer/Payment.java
+++ src/main/java/PresentationLayer/Payment.java
@@ -15,35 +15,42 @@ public class Payment extends Command {
         HttpSession session = request.getSession();
         ServletContext context = request.getSession().getServletContext();
 
+
         User user = (User) context.getAttribute("user");
-        double totalPrice = (double) session.getAttribute("basketprice");
-
-        if(user.getCredit() >= totalPrice) {
-            String email = user.getEmail();
-            int customerId = LogicFacade.getCustomerId(email);
-            LogicFacade.createOrder(customerId);
-
-            double sum = (double) session.getAttribute("price");
-            int orderId = LogicFacade.getOrderId(user.getId());
-            int qty = (int) session.getAttribute("qty");
-
-            Topping top = (Topping) session.getAttribute("top");
-            Bottom bot = (Bottom) session.getAttribute("bot");
-            int topping_id = top.getId();
-            int bottom_id = bot.getId();
-
-            LogicFacade.addToBasket(orderId, qty, (int) sum, topping_id, bottom_id);
-            String accepted = "DIN ORDRER ER BLEVET BETALT OG KAN HENTES OM 10 MIN";
-            request.setAttribute("message", accepted);
-
-            int updatedCredit = (int) (user.getCredit() - totalPrice);
-            ProductMapper.updateCredit(email,updatedCredit);
-
-            user.setCredit(updatedCredit);
-            session.setAttribute("credit", user.showBalance());
+        if(user == null){
+            request.setAttribute("message","HOV, HUSK AT LOGGE IND INDEN DU KLIKKER BETAL");
+            return "checkout";
         } else {
-            String rejected = "DER ER ET PROBLEM MED DIN ØKONOMISKE SITUATION";
-            request.setAttribute("message", rejected);
+            double totalPrice = (double) session.getAttribute("basketprice");
+
+            if (user.getCredit() >= totalPrice) {
+                String email = user.getEmail();
+                int customerId = LogicFacade.getCustomerId(email);
+                LogicFacade.createOrder(customerId);
+
+                double sum = (double) session.getAttribute("price");
+                int orderId = LogicFacade.getOrderId(user.getId());
+                int qty = (int) session.getAttribute("qty");
+
+                Topping top = (Topping) session.getAttribute("top");
+                Bottom bot = (Bottom) session.getAttribute("bot");
+                int topping_id = top.getId();
+                int bottom_id = bot.getId();
+
+                LogicFacade.addToBasket(orderId, qty, (int) sum, topping_id, bottom_id);
+                String accepted = "DIN ORDRER ER BLEVET BETALT OG KAN HENTES OM 10 MIN";
+                request.setAttribute("message", accepted);
+
+                int updatedCredit = (int) (user.getCredit() - totalPrice);
+                ProductMapper.updateCredit(email, updatedCredit);
+
+                user.setCredit(updatedCredit);
+                session.setAttribute("credit", user.showBalance());
+
+            } else {
+                String rejected = "DER ER ET PROBLEM MED DIN ØKONOMISKE SITUATION";
+                request.setAttribute("message", rejected);
+            }
         }
 
         return "checkout";
